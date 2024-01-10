cf81c45f219056de163427b0ec731b4f56c02b5d
kristinholda <k.holda@yahoo.com>
2024-01-10 14:55:51 -0500

    updated code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("");
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response);
   }
 
   function search(event) {
