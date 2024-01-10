dfa06c83a22f2679f8780b648a50f21f3f8bda10
kristinholda <k.holda@yahoo.com>
2024-01-10 14:53:05 -0500

    added dictionary interggration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Seaching for ${keyWord}`);
+
+    let apiKey = "4126dae244a02f134d2t0cdo0b2944b0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyWordChange(event) {
