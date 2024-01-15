9fdb44e7c164b1b1b3762bd32a7ccfb1437665e0
kristinholda <k.holda@yahoo.com>
2024-01-15 13:17:10 -0500

    Added Synonyms integration

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech} </h3>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p>
+        <strong>Definition:</strong> {props.meaning.definition}
+      </p>
+      <p>
+        <strong>Example:</strong> <em>{props.meaning.example}</em>
+      </p>
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
---
+++ src/Synonyms.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <p>
+          <strong>Synonyms:</strong>
+        </p>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
